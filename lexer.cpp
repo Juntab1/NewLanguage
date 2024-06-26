502adf02a44640e37c820cd7eb21959c396173eb
Juntab1 <jsbwhaleguy@gmail.com>
2024-06-26 13:36:43 -0700

    initial lexer complete

--- lexer.cpp
+++ lexer.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <map>
+#include <cstring>
+#include <fstream>
+
 
 // using namespace std;
 
@@ -76,6 +80,15 @@ bool isSkippable(char ch)
     return ch == ' ' || ch == '\t' || ch == '\n';
 }
 
+// Find if it is a recognizable identifier
+typedef std::map<std::string, TokenType> ReservedIdentifierMap;
+ReservedIdentifierMap reservedIdentifier;
+
+void INIT_RESERVED_IDENTIFIER()
+{
+    reservedIdentifier["let"] = TokenType::Let;
+}
+
 
 
 // function takes source code as input and returns a vector containing tokens
@@ -146,8 +159,9 @@ std::vector<Token> tokenize(const std::string &sourceCode)
                 std::string identifier = shift(src);
 
                 // implement ReservedIdentMap later
-                ReservedIdentMap::iterator it = reservedIdent.find(identifier);
-                if (it != reservedIdent.end())
+                ReservedIdentifierMap::iterator it = reservedIdentifier.find(identifier);
+                // the .end() points to a non-existant end iterator 
+                if (it != reservedIdentifier.end())
                 {
                     tokens.push_back(Token::token(identifier, it->second));
                 }
@@ -172,9 +186,63 @@ std::vector<Token> tokenize(const std::string &sourceCode)
     return tokens;
 }
 
+// STILL DON'T REALLY KNOW WHAT ARGC AND ARGV IS FOR
+// argc = number of commandline arguments passed to the program, always atleast 1
+// argv = array of character arrays: argv[0] is the name of the program
+int main(int argc, char *argv[])
+{
+    // check if the arguments are correct
+    if (argc != 2)
+    {
+        // cerr is error stream
+        std::cerr << "Incorrect Arguments" << std::endl;
+        std::cerr << "Correct usage: ./dejavu <input file path --> input.vu>" << std::endl;
+    }
+
+    // checking if the input file is valid -> .vu ?
+    // this block is called a compound statement or scope blocks
+    {
+        const char *ext = ".vu";
+        size_t xlen = strlen(ext);
+        size_t slen = strlen(argv[1]);
+        // making sure the ext is the same
+        int found = strcmp(argv[1] + slen - xlen, ext) == 0;
+        if (found == 0)
+        {
+            std::cerr << "Invalid code file" << std::endl;
+            return EXIT_FAILURE;
+        }
+    }
+
+    // initialize the variable
+    INIT_RESERVED_IDENTIFIER();
+
+    // read input source code file and convert it into a string stream
+
+    // end goal is to put the code into sourceCode as a string
+    std::string sourceCode;
+    {
+        // allows insertion, deletion, and such
+        std::stringstream contents_stream;
+        // it can write, read, and such from files
+        // std::ios::in means it should be open in input mode
+        std::fstream input(argv[1], std::ios::in);
+        // get pointer to the buffer
+        contents_stream << input.rdbuf();
+        sourceCode = contents_stream.str();
+    }
+
+    std::vector<Token> tokens = tokenize(sourceCode);
+    // we are using pre increment for i because the first value might not be important because it can be 
+    // something like a placeholder for start.
+    for (int i = 0; i < tokens.size(); ++i)
+    {
+        std::cout << "Value:" << tokens[i].value << " Type: " << tokens[i].type << std::endl;
+    }
+
+}
+
 // Tommorow:
-// - implement reservedIdentMap 
-// - implement main
 
 // I want to make my programming language:
 // - not have any identifier like let or string or whatever like Python, this is great practice to be able to implement tests and such
